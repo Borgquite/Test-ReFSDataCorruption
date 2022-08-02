99466c6ae34e39cb87697b7c89975331fc853bf9
Chris Hill <53898223+Borgquite@users.noreply.github.com>
2022-08-02 12:27:21 +0100

    Update version number

--- Test-ReFSDataCorruption.ps1
+++ Test-ReFSDataCorruption.ps1
@@ -1,4 +1,4 @@
-# Test ReFS data corruption detection (Test-ReFSDataCorruption.ps1) version 1.5
+# Test ReFS data corruption detection (Test-ReFSDataCorruption.ps1) version 1.6
 
 # Public domain. You may copy, modify, distribute and perform any parts of this work not covered under the sources below without asking permission under CC0 1.0 Universal (https://creativecommons.org/publicdomain/zero/1.0/)
 # Based on an original script by kjo at deif dot com - https://forums.veeam.com/veeam-backup-replication-f2/refs-data-corruption-detection-t53098.html#p345182
